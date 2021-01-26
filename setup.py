0a7f7a04bd60e3c83ae943098d28b76889b8aa22
Lucas Carlson <lucas@carlson.net>
2021-01-26 14:37:01 -0800

    Fixing readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cloudenv", # Replace with your own username
-    version="0.1.0",
+    version="0.1.1",
     author="Lucas Carlson",
     author_email="lucas@carlson.net",
     description="Keep Your Environmental Variables Secure And In Sync",
