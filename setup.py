e583dbefe5283a13cd1460e6d60b67a8f9f72a56
Lucas Carlson <lucas@carlson.net>
2021-01-26 14:47:36 -0800

    Fixing the packaging script

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
+import io
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+meta = {}
+with io.open('./src/cloudenv/version.py', encoding='utf-8') as f:
+    exec(f.read(), meta)
+
 setuptools.setup(
     name="cloudenv", # Replace with your own username
-    version="0.1.1",
+    version=meta['__version__'],
     author="Lucas Carlson",
     author_email="lucas@carlson.net",
     description="Keep Your Environmental Variables Secure And In Sync",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cloudenvhq/cloudenv-python/",
-    packages=setuptools.find_packages(),
+    packages=['cloudenv'],
+    package_dir={'': 'src'},
+    install_requires=['python-dotenv>=0.15.0'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
